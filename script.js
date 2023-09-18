9411890e56162cb0e3af9a367a3399e9c11ab704
wnnow <117201813+wnnow@users.noreply.github.com>
2023-09-19 04:40:52 +0700

    Fixed UI, Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+  return operator(firstNumber, secondNumber);
+}
+
 function add(...args) {
   let sum = 0;
   for (let arg of args) {
@@ -5,6 +13,7 @@ function add(...args) {
   }
   return sum;
 }
+
 function minus(...args) {
   let sum = args[0];
   for (let i = 1; i < args.length; i++) {
@@ -28,3 +37,14 @@ function divide(...args) {
   }
   return sum;
 }
+
+const numberBtns = document.querySelectorAll(".number");
+numberBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+  })
+);
+
+// press button input number to store in first var until press other mathematic button
+// number go above
+// press mathematic button show mathematic
