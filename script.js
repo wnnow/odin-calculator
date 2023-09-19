7bf274818cb9bc25202a055b11c60b1d64b856e3
wnnow <117201813+wnnow@users.noreply.github.com>
2023-09-19 23:51:36 +0700

    Fixed number can chain calcurate

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const resetBtn = document.querySelector("#reset-btn");
-
 const ceBtn = document.querySelector("#back-btn");
 const displayValueContainer = document.querySelector(
   ".display-value-container"
 );
+const displayValueLastChild = displayValueContainer.lastElementChild;
+const numberBtns = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const resultBtn = document.querySelector("#result-btn");
 let firstNumber;
 let operator;
 let secondNumber;
 let displayValue;
+let arrOperators = ["+", "-", "÷", "×"];
 
 function operate(firstNumber, operator, secondNumber) {
   return operator(firstNumber, secondNumber);
@@ -45,36 +49,8 @@ function divide(...args) {
   return sum;
 }
 
-const numberBtns = document.querySelectorAll(".number");
-numberBtns.forEach((btn) =>
-  btn.addEventListener("click", (e) => {
-    displayValue = displayValueContainer.lastElementChild.textContent +=
-      e.target.textContent;
-  })
-);
-
-const operators = document.querySelectorAll(".operator");
-operators.forEach((btn) =>
-  btn.addEventListener("click", (e) => {
-    if (isNaN(+displayValue.slice(displayValue.length - 1))) {
-      deleteLastCha();
-    }
-    firstNumber = displayValue;
-    operator =
-      e.target.textContent === "+"
-        ? add
-        : e.target.textContent === "-"
-        ? subtract
-        : e.target.textContent === "×"
-        ? multiply
-        : divide;
-    displayValue = displayValueContainer.lastElementChild.textContent +=
-      e.target.textContent;
-  })
-);
-
-function checkOperator(str) {
-  let operator =
+function checkOperator(e) {
+  operator =
     e.target.textContent === "+"
       ? add
       : e.target.textContent === "-"
@@ -82,18 +58,54 @@ function checkOperator(str) {
       : e.target.textContent === "×"
       ? multiply
       : divide;
+  return operator;
 }
 
 function clearDisplay() {
-  displayValueContainer.lastElementChild.textContent = "";
+  displayValueLastChild.textContent = "";
 }
 
 function deleteLastCha() {
-  displayValueContainer.lastElementChild.textContent =
-    displayValueContainer.lastElementChild.textContent.slice(
-      0,
-      displayValueContainer.lastElementChild.textContent.length - 1
-    );
+  displayValueLastChild.textContent = displayValueLastChild.textContent.slice(
+    0,
+    displayValueLastChild.textContent.length - 1
+  );
 }
+
+function removeOperators() {
+  for (let arg of arrOperators) {
+    if (displayValueLastChild.textContent === arg) {
+      displayValueLastChild.textContent = "";
+    }
+  }
+}
+
 resetBtn.addEventListener("click", clearDisplay);
 ceBtn.addEventListener("click", deleteLastCha);
+
+numberBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    removeOperators();
+    displayValue = displayValueLastChild.textContent += e.target.textContent;
+  })
+);
+
+operators.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    firstNumber = Number(displayValueLastChild.textContent);
+    checkOperator(e);
+    displayValue = displayValueLastChild.textContent = e.target.textContent;
+  })
+);
+resultBtn.addEventListener("click", (e) => {
+  secondNumber = Number(displayValueLastChild.textContent);
+  displayValueLastChild.textContent = operate(
+    firstNumber,
+
+    operator,
+
+    secondNumber
+  );
+
+  firstNumber = displayValueLastChild.textContent;
+});
